dd6b2e821eec83b015bca960da8fdf1e2eb64bc9
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-05-24 16:38:14 +0530

    Refactor karl-kldl

--- karl-kldl/build.gradle.kts
+++ karl-kldl/build.gradle.kts
@@ -5,8 +5,10 @@ plugins {
 
 kotlin {
     jvm {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
         }
     }
 
@@ -17,6 +19,11 @@ kotlin {
                 implementation(libs.kotlinx.coroutines.core) // Or 'api' if interfaces here use coroutine types
             }
         }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
         val jvmMain by getting {
             dependencies {
                 implementation(libs.kotlin.stdlib.jdk8)
@@ -24,6 +31,9 @@ kotlin {
                 implementation(libs.kotlindl.dataset)
                 // implementation(libs.kotlindl.tensorflow) // If needed
             }
+        }
+        val jvmTest by getting {
+            dependsOn(commonTest)
         }
     }
 }
