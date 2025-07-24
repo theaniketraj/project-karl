cdafec3ea7cfb8baae39c693d60a1cd61ee1dab0
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-07-24 11:27:35 +0530

    Update Kotlin serialization plugin version to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.compose") version "1.8.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
