0979eb56a936c419f13a5259dc5c8f2fdfe948b1
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-07-24 21:23:32 +0530

    Update Kotlin plugin versions to 2.0.21 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 // karl-project/build.gradle.kts
 
 plugins {
-    id("org.jetbrains.kotlin.multiplatform") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.0.21" apply false
     id("org.jetbrains.compose") version "1.8.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
