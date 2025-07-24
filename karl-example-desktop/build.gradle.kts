92e7bf60d8fb5b96ea9132d8e94037f6f61a9050
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2025-07-24 21:23:17 +0530

    Update JVM target version to 21 in build.gradle.kts

--- karl-example-desktop/build.gradle.kts
+++ karl-example-desktop/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = "17" // Set the JVM target version
+        jvmTarget = "21" // Set the JVM target version
     }
 }
 
